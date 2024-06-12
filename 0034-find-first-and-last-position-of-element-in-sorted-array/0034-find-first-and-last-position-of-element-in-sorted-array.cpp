12f45c878433bb8bda7215d6405c0304a8c9ab52
Raksha Madhogaria <128985351+27Raksha@users.noreply.github.com>
2024-06-12 13:13:51 +0530

    Time: 6 ms (50.22%), Space: 15.9 MB (98.43%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,12 +4,9 @@ public:
       vector<int> ans(2, -1);
     int n = nums.size();
 
-    // Edge case: empty array
     if (n == 0) {
         return ans;
     }
-
-    // Find the first occurrence of the target
     for (int i = 0; i < n; ++i) {
         if (nums[i] == target) {
             ans[0] = i;
@@ -17,12 +14,10 @@ public:
         }
     }
 
-    // If the target was not found, return [-1, -1]
     if (ans[0] == -1) {
         return ans;
     }
 
-    // Find the last occurrence of the target
     for (int i = n - 1; i >= 0; --i) {
         if (nums[i] == target) {
             ans[1] = i;
